4846738ab41bd9f41b6946fb240b70b8125eb079
matheuspaiva1 <matheuspaiva0007@gmail.com>
2024-11-11 16:24:05 -0300

    criando model do sequelize

---
+++ config.js
@@ -0,0 +1,8 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize('to-do','postgres','paiva0007', {
+  host: 'localhost',
+  dialect: postgres,
+})
+
+module.exports = sequelize;
---
+++ src/entities/task.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require("sequelize");
+
+const sequelize = require("/config.js");
+
+const Task = sequelize.define('User', {
+  id:{
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  description:{
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  checked:{
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  }, 
+}, {
+  tableName: "task"
+})
+
+module.exports = Task
