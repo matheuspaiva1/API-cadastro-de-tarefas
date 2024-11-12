d19f037d95ca3434d5bee030f377a9d68180362d
matheuspaiva1 <matheuspaiva0007@gmail.com>
2024-11-12 14:57:00 -0300

    configurando rota POST

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-// index.js
-const sequelize = require('./config.js');
-const Task = require('./src/entities/task.js');
+const express = require('express');
+const sequelize = require('./config'); 
+const Task = require('./src/entities/task');
+const router = require('./src/routes/taskRoutes');
+
+const app = express();
+
+app.use(express.json()); 
+
+app.use('/api', router);
 
 sequelize.sync()
   .then(() => {
-    console.log('Banco de dados sincronizado.');
+    console.log('Banco de dados sincronizado');
+    app.listen(3000, () => {
+      console.log('Servidor rodando na porta 3000');
+    });
   })
-  .catch((error) => {
-    console.error('Erro ao sincronizar o banco de dados:', error);
-  });
+  .catch((error) => console.error('Erro ao sincronizar o banco de dados:', error));
---
+++ src/routes/taskRoutes.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const Task = require('../entities/task');
+
+const router = express.Router();
+
+router.post('/task', async (req, res) => {
+  try {
+    const task = await Task.create(req.body)
+    res.status(201).json(task)
+  } catch (error) {
+    res.status(400).json({ error: error })
+  }
+})
+
+router.get('/task', async (req, res) => {
+  try {
+    const task = await Task.create(req.body)
+    res.status(201).json(task)
+  } catch (error) {
+    res.status(400).json({ error: error })
+  }
+})
+
+router.patch('/task:id', async (req, res) => {
+  try {
+    const task = await Task.create(req.body)
+    res.status(201).json(task)
+  } catch (error) {
+    res.status(400).json({ error: error })
+  }
+})
+
+router.delete('/task:id', async (req, res) => {
+  try {
+    const task = await Task.create(req.body)
+    res.status(201).json(task)
+  } catch (error) {
+    res.status(400).json({ error: error })
+  }
+})
+
+module.exports = router
